2a6195a3df24bd269ab6ba4bd34ca99c53c598e8
KnightryryV2 <116779059+KnightryryV2@users.noreply.github.com>
2023-01-26 12:08:26 -0500

    more testing

--- Games/CustomGame/script.js
+++ Games/CustomGame/script.js
@@ -2,7 +2,7 @@
 import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
 
 // Start kaboom with default options (will create a fullscreen canvas under <body>)
-kaboom()
+kaboom();
 
 // Init with some options (check out #KaboomOpt for full options list)
 kaboom({
@@ -11,10 +11,10 @@ kaboom({
     font: "sinko",
     canvas: document.querySelector("#mycanvas"),
     background: [ 0, 0, 255, ],
-})
+});
 
 // All kaboom functions are imported to global after calling kaboom()
-add()
-onUpdate()
-onKeyPress()
-vec2()
+add();
+onUpdate();
+onKeyPress();
+vec2();
